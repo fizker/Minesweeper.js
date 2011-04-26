f70490a03af48ec669e1a5bb17b94aedfe6006d1
Benjamin Horsleben <benjamin@horsleben.com>
2011-04-26 17:53:15 +0200

    Updated README with node.js instructions. Updated the server-code to allow auto-concatenating js- and css-files on the fly, when a concatenated file is not already on the server.

--- js/server.js
+++ js/server.js
@@ -63,35 +63,72 @@
 		handlers[path] = handler;
 	};
 	
-	
-	
-	function staticHandler(filePath) {
-		var headers, body, contentType,
+	function staticHandler(preferredFile, fallbackFiles) {
+		var headers = { 'Content-type': mimeTypes.parse(preferredFile), 'Content-length': 0 }, 
+			body = [], contentType, files,
 			debug = this.debug;
-		contentType = mimeTypes.parse(filePath);
+		
 		function loadResponseData(callback) {
 			if(body && headers && !debug) {
 				callback();
 				return;
 			}
-			
-			readFile(filePath, function(err, data) {
-				if(err) {
-					sys.puts('Error when loading file <'+filePath+'>: '+err);
-					body = headers = undefined;
-				} else {
-					body = data;
-					headers = {
-						'Content-type': contentType,
-						'Content-Length': body.length
-					}
-					if(!debug) {
-						headers['Cache-Control'] = 'public';
-					}
-					callback();
+			if(fallbackFiles) {
+				files = fallbackFiles.concat().reverse();
+			}
+			body = [];
+			function allDone() {
+				body = body.join("\n");
+				headers['Content-length'] = body.length;
+				if(!debug) {
+					headers['Cache-Control'] = 'public';
 				}
-			});
+				callback();
+			}
+			
+			function readFileCallback(err, data) {
+				if(err) {
+					this.error(err);
+					return;
+				}
+				this.success(data);
+			};
+			readFile(preferredFile, readFileCallback.bind({
+				error: function() {
+					var rfc;
+					
+					if(!fallbackFiles) {
+						sys.puts('Error when loading file <'+filePath+'>: '+err);
+						return;
+					}
+					
+					rfc = readFileCallback.bind({
+						error: function(err) {
+							sys.puts('Error when loading file <'+filePath+'>: '+err);
+							fallbackLoader();
+						},
+						success: function(data) {
+							body.push(data);
+							fallbackLoader();
+						}
+					});
+					function fallbackLoader() {
+						var file = files.pop();
+						if(file) {
+							readFile(file, rfc);
+						} else {
+							allDone();
+						}
+					};
+					fallbackLoader();
+				},
+				success: function(data) {
+					body.push(data);
+					allDone();
+				}
+			}));
 		};
+		
 		return function staticHandler(request, response) {
 			loadResponseData(function staticCallback() {
 				response.writeHead(200, headers);
